f63140da02180a4d4512de80ae7ff8172fe51815
Sanya Oluwadare <sanyaodare@gmail.com>
2021-09-13 11:53:07 +0100

    add modal close button

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const modalBtn = document.querySelector('.modal-btn');
+const modalOverlay = document.querySelector('.modal-overlay');
+const closeBtn = document.querySelector('.close-btn');
 
+// listen for click events on modal button
+modalBtn.addEventListener('click', function() {
+  modalOverlay.classList.add('open-modal');
+})
+
+// listen for click events on close button
+closeBtn.addEventListener('click', function() {
+  modalOverlay.classList.remove('open-modal');
+})
